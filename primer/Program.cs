4164128fdb541dc924af22db160dd2730cbe089a
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-01 22:03:55 +0300

    1 version

---
+++ primer/Program.cs
@@ -0,0 +1,77 @@
+﻿// //Имеется числовой массив A заполненный числами из отрезка 
+//[minValue; maxValue]. Создать на его основе масив B, отбрасывая те, 
+//которые нарушают порядок
+// возрастания
+// элементы, больше 8
+// знакочередования
+
+int minValue=10;
+int maxValue=100;
+Console.WriteLine ("Введите мин значение:");
+minValue = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите макс значение:");
+maxValue = Convert.ToInt32(Console.ReadLine ());
+
+
+int GetRandom ()
+{
+  return new Random().Next(minValue,maxValue);
+  }
+int i=0;
+
+int[] A = new int [10];
+while (i<10)
+{
+//A[i]= new Random().Next(10,100);
+A[i]=GetRandom();
+i++;
+}
+// печать массива А
+i=0;
+while (i<10)
+{ Console.Write(A[i]+" "); i++;}
+Console.WriteLine();
+
+
+
+
+i=0;
+int[] B = new int [10];
+while (i<10)
+{
+  if (A[i]>80) 
+  {
+  B[i]= A[i+1];
+  }
+  i++;
+}
+
+i=0;
+while (i<10)
+{
+Console.Write(A[i]+" ");
+i++;
+}
+
+// печать нового массива B
+i=0;
+while (i<10)
+{
+Console.Write(B[i]+" ");
+i++;
+}
+// while (i<=9)
+// {
+//     Con
+// }
+
+
+
+
+
+
+
+
+
+
+
